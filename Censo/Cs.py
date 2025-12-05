5fed8187bcdce6c97e56d928ec6c2dd481cb9db8
DLJ7666 <jarguezduran@gmail.com>
2025-12-05 14:11:03 +0100

    adaptado C1 para que incluya las 3 cs
    añadido también registros de certificados

---
+++ Censo/Cs.py
@@ -0,0 +1,59 @@
+import rsa
+from fastapi import FastAPI, Header, HTTPException
+
+from firmar import firmar
+
+appC = FastAPI()
+
+clave_publica_C1, clave_privada_C1 = rsa.newkeys(512)
+clave_publica_C2, clave_privada_C2 = rsa.newkeys(512)
+clave_publica_C3, clave_privada_C3 = rsa.newkeys(512)
+registro_C1 = set()
+registro_C2 = set()
+registro_C3 = set()
+
+@appC.get("/public_key/{c_id}")
+def get_public_key(c_id: int):
+    if c_id == 1:
+        return {
+            "n": clave_publica_C1.n,
+            "e": clave_publica_C1.e
+        }
+    elif c_id == 2:
+        return {
+            "n": clave_publica_C2.n,
+            "e": clave_publica_C2.e
+        }
+    elif c_id == 3:
+        return {
+            "n": clave_publica_C3.n,
+            "e": clave_publica_C3.e
+        }
+    else:
+        raise HTTPException(status_code=404, detail="Clave pública no encontrada")
+
+@appC.get("/identify/{c_id}")
+def identify(c_id: int, mensaje : int = Header(...), certificado: str = Header(...)):
+    clave_privada = None
+    if c_id == 1:
+        if certificado in registro_C1:
+            raise HTTPException(status_code=400, detail="Certificado ya utilizado para C1")
+        else :
+            registro_C1.add(certificado)
+            clave_privada = clave_privada_C1
+    elif c_id == 2:
+        if certificado in registro_C2:
+            raise HTTPException(status_code=400, detail="Certificado ya utilizado para C2")
+        else:
+            registro_C2.add(certificado)
+            clave_privada = clave_privada_C2
+    elif c_id == 3:
+        if certificado in registro_C3:
+            raise HTTPException(status_code=400, detail="Certificado ya utilizado para C3")
+        else:
+            registro_C3.add(certificado)
+            clave_privada = clave_privada_C3
+    else:
+        raise HTTPException(status_code=404, detail="Clave privada no encontrada")
+    
+    return {"message": firmar(mensaje, clave_privada)}
--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
-echo "Iniciando C1"
-uvicorn C1:app  --reload --port 8800 &
-echo "Iniciando C2"
-uvicorn C2:app  --reload --port 8801 &
-echo "Iniciando C3"
-uvicorn C3:app  --reload --port 8802 &
+echo "Iniciando el censo"
+uvicorn C:app  --reload --port 8800 &
 echo "Iniciando VS"
 uvicorn VS:app  --reload --port 8888 &
